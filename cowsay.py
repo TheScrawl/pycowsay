178db3eeb768d659bdb95ed30bc4518a9ca92fcd
TheScrawl <thescrawlgaming@gmail.com>
2017-09-20 13:17:50 +1200

    Add files via upload

---
+++ cowsay.py
@@ -0,0 +1,11 @@
+def say(word):
+    print(' ' + '_'*(len(str(word)) + 2))
+    print('< ' + word + ' >')
+    print(' ' + '-'*(len(str(word)) + 2))
+    print('''        \   ^__^
+         \  (oo)\_______
+            (__)\       )\\/\\
+                ||----w |
+                ||     ||''')
+
+say(input())
